5ebb9766c3c4bb4698aa0be52fb517451f849588
Noel vanSchaick <johnonymous4@gmail.com>
2026-01-23 13:23:14 -0800

    added implementation and operations for ExpressionVar

--- clawgic/src/expression_tree/expression_var.rs
+++ clawgic/src/expression_tree/expression_var.rs
@@ -1,3 +1,5 @@
+use std::ops::{BitAnd, BitAndAssign, BitOr, BitOrAssign, Not, Shl, ShlAssign, Shr, ShrAssign};
+
 use crate::expression_tree::ExpressionTree;
 
 #[derive(Clone, Debug)]
@@ -6,6 +8,9 @@ pub struct ExpressionVar{
     expr: ExpressionTree,
 }
 
+/// Atomic Variable for an ExpressionTree.
+/// 
+/// Not necessary for constructing a tree, but very helpful.
 impl ExpressionVar{
     ///Constructs and returns an ExpressionVar iff a valid name is given.
     pub fn new(name: String) -> Result<ExpressionVar, ()>{
@@ -23,5 +28,126 @@ impl ExpressionVar{
         }
 
         Ok(Self {expr:  ExpressionTree::new(&name).unwrap(), name})
+    }
+
+    ///Constructs a vec of ExpressionVars enumerated with the given range iff a valid name is given.
+    pub fn new_vars(name: String, range: std::ops::Range<usize>) -> Result<Vec<ExpressionVar>, ()>{
+        let mut vars = Vec::with_capacity(range.end - range.start);
+        for i in range{
+            match Self::new(name.clone() + &i.to_string()){
+                Ok(v) => vars.push(v),
+                Err(()) => return Err(())
+            }
+        }
+
+        Ok(vars)
+    }
+
+    ///Returns a reference to the name of the ExpressionVar
+    pub fn name(&self) -> &str{
+        &self.name
+    }
+
+    /// Returns an ExpressionTree containing only the variable.
+    /// 
+    /// Clones an ExpressionTree with each call, but it's a very inexpensive clone.
+    pub fn expr(&self) -> ExpressionTree{
+        self.expr.clone()
+    }
+}
+
+impl BitAnd<ExpressionTree> for &ExpressionVar{
+    type Output = ExpressionTree;
+
+    fn bitand(self, rhs: ExpressionTree) -> Self::Output {
+        self.expr() & rhs
+    }
+}
+
+impl BitAnd<&ExpressionVar> for ExpressionTree{
+    type Output = ExpressionTree;
+
+    fn bitand(self, rhs: &ExpressionVar) -> Self::Output {
+        self & rhs.expr()
+    }
+}
+
+impl BitAndAssign<&ExpressionVar> for ExpressionTree{
+    fn bitand_assign(&mut self, rhs: &ExpressionVar) {
+        *self &= rhs.expr();
+    }
+}
+
+impl BitOr<ExpressionTree> for &ExpressionVar{
+    type Output = ExpressionTree;
+
+    fn bitor(self, rhs: ExpressionTree) -> Self::Output {
+        self.expr() | rhs
+    }
+}
+
+impl BitOr<&ExpressionVar> for ExpressionTree{
+    type Output = ExpressionTree;
+
+    fn bitor(self, rhs: &ExpressionVar) -> Self::Output {
+        self | rhs.expr()
+    }
+}
+
+impl BitOrAssign<&ExpressionVar> for ExpressionTree{
+    fn bitor_assign(&mut self, rhs: &ExpressionVar) {
+        *self |= rhs.expr();
+    }
+}
+
+impl Not for &ExpressionVar{
+    type Output = ExpressionTree;
+
+    fn not(self) -> Self::Output {
+        !self.expr()
+    }
+}
+
+impl Shl<ExpressionTree> for &ExpressionVar{
+    type Output = ExpressionTree;
+
+    fn shl(self, rhs: ExpressionTree) -> Self::Output {
+        self.expr() << rhs
+    }
+}
+
+impl Shl<&ExpressionVar> for ExpressionTree{
+    type Output = ExpressionTree;
+
+    fn shl(self, rhs: &ExpressionVar) -> Self::Output {
+        self << rhs.expr()
+    }
+}
+
+impl ShlAssign<&ExpressionVar> for ExpressionTree{
+    fn shl_assign(&mut self, rhs: &ExpressionVar) {
+        *self <<= rhs.expr();
+    }
+}
+
+impl Shr<ExpressionTree> for &ExpressionVar{
+    type Output = ExpressionTree;
+
+    fn shr(self, rhs: ExpressionTree) -> Self::Output {
+        self.expr() >> rhs
+    }
+}
+
+impl Shr<&ExpressionVar> for ExpressionTree{
+    type Output = ExpressionTree;
+
+    fn shr(self, rhs: &ExpressionVar) -> Self::Output {
+        self >> rhs.expr()
+    }
+}
+
+impl ShrAssign<&ExpressionVar> for ExpressionTree{
+    fn shr_assign(&mut self, rhs: &ExpressionVar) {
+        *self >>= rhs.expr();
     }
 }
--- clawgic_tests/src/main.rs
+++ clawgic_tests/src/main.rs
@@ -3,7 +3,11 @@ use std::error::Error;
 use clawgic::expression_tree::ExpressionTree;
 
 mod expression_tree_tests;
+
+mod expression_var_tests;
+
 mod node_tests;
+
 
 fn main() -> Result<(), Box<dyn Error>>{
     ExpressionTree::new("")?;
